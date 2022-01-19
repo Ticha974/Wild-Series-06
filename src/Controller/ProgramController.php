350f3e84da2b23e781db76fb674a6814db5764fa
Ticha974 <natacha.crepu@hotmail.fr>
2022-01-19 09:40:04 +0100

    quest 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +17,13 @@ class ProgramController extends AbstractController
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
+    }
+
+    /**
+     * @Route("/program/{id<\d+>}", methods={"GET"}, name="program_show")
+     */
+    public function show(int $id)
+    {
+        return $this->render('/program/show.html.twig', ['id' => $id]);
     }
 }
